4897d5968469a6a548d9c8cdd0e97072a3eb14d5
unknown <abdouraoufm97@gmail.com>
2023-04-16 16:32:00 +0100

    first commit

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,26 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'etudiant',
+        data: { pageTitle: 'Etudiants' },
+        loadChildren: () => import('./etudiant/etudiant.module').then(m => m.EtudiantModule),
+      },
+      {
+        path: 'pays',
+        data: { pageTitle: 'Pays' },
+        loadChildren: () => import('./pays/pays.module').then(m => m.PaysModule),
+      },
+      {
+        path: 'filiere',
+        data: { pageTitle: 'Filieres' },
+        loadChildren: () => import('./filiere/filiere.module').then(m => m.FiliereModule),
+      },
+      {
+        path: 'niveau',
+        data: { pageTitle: 'Niveaus' },
+        loadChildren: () => import('./niveau/niveau.module').then(m => m.NiveauModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
